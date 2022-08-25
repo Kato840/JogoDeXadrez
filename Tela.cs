c5098964a5818f6dd02ad036f898ac56deecb5a6
Marco Antonio <marco_apjr@hotmail.com>
2022-08-25 04:33:57 -0300

    Implementando a marcação numerica e alfabética no tabuleiro

--- Tela.cs
+++ Tela.cs
@@ -10,16 +10,25 @@ internal class Tela
 {
     public static void ImprimirTabuleiro(Tabuleiro tabuleiro)
     {
-        for(int i=0; i<tabuleiro.Linhas; i++)
+        string[] a = new string[] { "A", "B", "C", "D", "E", "F", "G", "H" };
+        for (int i = 0; i < tabuleiro.Linhas; i++)
         {
-            for(int j=0; j<tabuleiro.Colunas; j++)
+            Console.Write(a[i] + "  ");
+            for (int j = 0; j < tabuleiro.Colunas; j++)
             {
-                if(tabuleiro.Peca(i,j) == null)
+                if (tabuleiro.Peca(i, j) == null)
                     Console.Write("_ ");
                 else
-                Console.Write( tabuleiro.Peca(i,j) +" ");
+                    Console.Write(tabuleiro.Peca(i, j) + " ");
             }
             Console.WriteLine();
         }
+        Console.WriteLine();
+        Console.Write("   ");
+        for (int j = 0; j < tabuleiro.Colunas; j++)
+        {
+            Console.Write($"{j+1}"+" ");
+        }
+        Console.WriteLine();
     }
 }
